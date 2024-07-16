42667e17dc99eb82635ac7f0b5f2f8b3d3b7cd73
Tanveer <tanveerdalal11@gmail.com>
2024-07-16 06:45:22 +0100

    Chapter 1 - Introduction

---
+++ Learn SQL using MySQL in one day/Chapter 1 - Introduction.sql
@@ -0,0 +1,25 @@
+# 2 Ways of making comments in SQL
+
+# What is SQl?
+/* SQL stands for Structured Query Language and is a language 
+   used to manage data stored in a relational database. */
+   
+# What is database?
+/* A database is a collection of data organized in some format
+   so that the data can be easily accessed, managed and updated.
+   Relational database organize data in the form of tables.
+   In addition, they contain queries, views and other elements
+   to help us interact with the data. */
+   
+# What is DBMS?
+/* In order to manage our database, we need to use a software 
+application known as a database management system (DBMS). */
+
+# What we learned so far?
+/* 1) SQL is a language
+   2) A database is a structured collection of data
+   3) A DBMS is a software that we use to manage our database
+*/
+
+SELECT "Hello World";
+SELECT "My SQL is Fun";
